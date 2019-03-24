cdf915eb6a063a8a68a8fb8fb8b5fa330fcd3518
David Agustin <davidsyagustin@gmail.com>
2019-03-24 13:34:42 -0700

    make it better

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const port = Number(process.env.PORT || 4000);
+
+const app = express();
+
 express.static(path.join(__dirname, './../client/dist'));
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
