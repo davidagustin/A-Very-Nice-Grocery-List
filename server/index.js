74ece22289e16dc2275815e329637620557caa24
David Agustin <davidsyagustin@gmail.com>
2019-03-24 14:02:11 -0700

    test

--- server/index.js
+++ server/index.js
@@ -1,28 +1,14 @@
 const express = require('express');
 const path = require('path');
-// const http = require('http');
 
-const port = Number(process.env.PORT || 4000);
+const port = Number(process.env.PORT || 5000);
 
 const app = express();
 
-process.env.PWD = process.cwd();
+app.set('port', process.env.PORT || 5000);
 
-app.set('views', path.join(process.env.PWD, './../client/dist'));
+app.use(express.static(path.join(__dirname, './../client/dist')));
 
-app.use(express.static(path.join(process.env.PWD, './../client/dist')));
-
-// app.get('/', (req, res) => {
-//   express.static(path.join(__dirname, './../client/dist'));
-// });
-
-// express.static(path.join(__dirname, './../client/dist'));
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
